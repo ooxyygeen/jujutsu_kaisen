0676217522fd846dafe6ec7673334dff13343d90
oxygen <ooxyygen0@gmail.com>
2021-11-30 00:51:05 +0200

    fix file management and add some methods to Character class and its subclasses; create test for fight

--- src/Shikigami.java
+++ src/Shikigami.java
@@ -7,4 +7,11 @@ public class Shikigami extends Stats {
         this.possibilityOfBeing = false;
         this.name = "Shikigamus";
     }
+
+    public Shikigami(int aStrength, int aIntel, int aLuck, int aAgility, int aHealth, int aEnergy,
+                     boolean aPossibility, String aName) {
+        super(aStrength, aIntel, aLuck, aAgility, aHealth, aEnergy);
+        this.possibilityOfBeing = aPossibility;
+        this.name = aName;
+    }
 }
