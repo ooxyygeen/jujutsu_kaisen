127b821c4318a6b58b680a6ffaee48ddbb686c8a
oxygen <ooxyygen0@gmail.com>
2022-06-28 19:22:31 +0300

    commit for Vitya

--- src/main/java/com/GUI/javacoursework/HelloApplication.java
+++ src/main/java/com/GUI/javacoursework/HelloApplication.java
@@ -11,16 +11,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-scene.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu-scene.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu-scene.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 1280, 720);
         String css = this.getClass().getResource("/css/style.css").toExternalForm();
         scene.getStylesheets().add(css);
-        GameWindowController gameWindowController = fxmlLoader.getController();
-        scene.setOnKeyPressed(gameWindowController::keyListener);
-//        MenuWindowController menuWindowController = fxmlLoader.getController();
-        stage.setTitle("Game");
+//        GameWindowController gameWindowController = fxmlLoader.getController();
+//        scene.setOnKeyPressed(gameWindowController::keyListener);
+        MenuWindowController menuWindowController = fxmlLoader.getController();
+        stage.setTitle("Jujutsu-Kaisen Game");
         stage.setScene(scene);
         stage.setMaxHeight(720);
         stage.setMaxWidth(1280);
--- src/main/java/com/GUI/javacoursework/MenuWindowController.java
+++ src/main/java/com/GUI/javacoursework/MenuWindowController.java
@@ -2,36 +2,75 @@ package com.GUI.javacoursework;
 
 import com.engine.javacoursework.*;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class MenuWindowController {
+    private int menuChoice;
     private Stage stage;
-    static String filename = "";
-    static Scanner s = new Scanner(System.in);
+    private String filename = "";
+    private String s;
+    @FXML
+    private Pane dialogPane;
+    @FXML
+    private Label dialogLabel;
+    @FXML
+    private TextField dialogErrorLabel;
+    @FXML
+    private TextField textField;
+
+    public void startButton(ActionEvent e) throws IOException {
+        filename = textField.getText();
+        if (textField.getLength() != 0) {
+            System.out.println(filename);
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-scene.fxml"));
+            Parent root = fxmlLoader.load();
+            stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            GameWindowController gameWindowController = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            scene.setOnKeyPressed(gameWindowController::keyListener);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 
     public void newGame(ActionEvent e) throws IOException {
-        System.out.println("Enter session name");
+        menuChoice = 1;
+        dialogLabel.setText("Enter session name");
+        dialogPane.setVisible(true);
+
+
+       /* System.out.println("Enter session name");
         s.nextLine();
         filename = s.nextLine();
         while (filename == null && filename.trim().isEmpty()) {
             System.out.println("Please, enter the session name.");
             s.nextLine();
             filename = s.nextLine();
-        }
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-scene.fxml"));
+        }*/
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-scene.fxml"));
         Parent root = fxmlLoader.load();
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         GameWindowController gameWindowController = fxmlLoader.getController();
         Scene scene = new Scene(root);
         scene.setOnKeyPressed(gameWindowController::keyListener);
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
+    }
+
+    public void saveGame(ActionEvent e) {
+
     }
 }
+
+
